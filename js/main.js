da665fd8c76fafd19c81d4d23c1a6c6e5b6f070b
ccamposcr <christian.campos@digitaslbi.com>
2017-01-16 22:08:59 -0600

    Gorilla test

--- js/main.js
+++ js/main.js
@@ -6,4 +6,9 @@
     							target:125
     						});
 
+    //You can set a new progress value following the next steps
+    var progressIndicator1 = $('.progress-indicator').first().data('_progressIndicator');
+    setTimeout(function(){
+    	progressIndicator1.setProgress(78);
+    },1000);
 })(jQuery);
